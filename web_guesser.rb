2a597cc70b82bc09007fa5206b99b0878e8ee52e
erikbenton <erik.benton@hotmail.com>
2017-12-14 14:22:13 -0500

    I4: adding background color based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,24 +5,31 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
 	if params["guess"]
-		message = check_guess(params["guess"].to_i)		
+		message, color = check_guess(params["guess"].to_i)		
 	else
 		message = ""
+		color = ""
 	end
 
-	erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message}
+	erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message, :color => color}
 end
 
 def check_guess(guess)
 	if guess - SECRET_NUMBER > 5
 		message = "Way too high!"
+		color = "red"
 	elsif guess > SECRET_NUMBER
 		message = "Too high!"
+		color = "orange"
 	elsif guess - SECRET_NUMBER < -5
 		message = "Way too low!"
+		color = "blue"
 	elsif guess < SECRET_NUMBER
 		message = "Too low!"
+		color = "lightblue"
 	elsif guess == SECRET_NUMBER
 		message = "You got it right!"
+		color = "green"
 	end
+	return message, color
 end
